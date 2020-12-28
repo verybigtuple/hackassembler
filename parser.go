53a9785ccbea796b273027fba6f95577f4e91592
verybigtuple <bigtuple@gmail.com>
2020-12-29 02:47:07 +0300

    Added Parser as struct in order to get comples error message

--- parser.go
+++ parser.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"strings"
 	"unicode"
 )
@@ -11,6 +13,15 @@ const (
 	inlineCommentDelim = '/'
 )
 
+type ParseError struct {
+	Pos int
+	Msg string
+}
+
+func (e *ParseError) Error() string {
+	return fmt.Sprintf("Position %d: %s", e.Pos, e.Msg)
+}
+
 // CIntstruction parsed into 3 parts
 type CIntstruction struct {
 	Dest string
@@ -18,33 +29,98 @@ type CIntstruction struct {
 	Jump string
 }
 
-// ParseCInstrunction returns pointer to a CInstruction struct parsed from a string line
-func ParseCInstrunction(input string) (*CIntstruction, error) {
-	ci := CIntstruction{}
-	b := strings.Builder{}
-	b.Grow(3)
+type Parser struct {
+	cInstr     CIntstruction
+	rReader    io.RuneReader
+	cmdBuilder strings.Builder
+	pos        int
+}
 
-	expectedPart := &ci.Comp
+func NewParser() *Parser {
+	p := Parser{cmdBuilder: strings.Builder{}}
+	p.cmdBuilder.Grow(3)
+	return &p
+}
+
+func (p *Parser) Parse(str string) (*CIntstruction, error) {
+	p.cInstr = CIntstruction{}
+	p.rReader = strings.NewReader(str)
+	p.cmdBuilder.Reset()
+	p.pos = 0
+
+	setter := p.setComp
 Loop:
-	for _, runeVal := range input {
-		switch {
-		case runeVal == compDelim:
-			ci.Dest = b.String()
-			b.Reset()
-		case runeVal == jumpDelim:
-			ci.Comp = b.String()
-			expectedPart = &ci.Jump
-			b.Reset()
-		case runeVal == inlineCommentDelim:
+	for {
+		rv, _, err := p.rReader.ReadRune()
+		if err != nil {
 			break Loop
-		case !unicode.IsSpace(runeVal):
-			b.WriteRune(runeVal)
+		}
+		p.pos++
+
+		switch {
+		case rv == compDelim:
+			err = p.setDest()
+		case rv == jumpDelim:
+			err = p.setComp()
+			setter = p.setJump
+		case rv == inlineCommentDelim:
+			err = p.parseComment()
+			if err != nil {
+				return nil, err
+			}
+			break Loop
+		case !unicode.IsSpace(rv):
+			p.cmdBuilder.WriteRune(rv)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	if p.cmdBuilder.Len() > 0 {
+		err := setter()
+		if err != nil {
+			return nil, err
 		}
 	}
 
-	if b.Len() > 0 {
-		*expectedPart = b.String()
-	}
+	return &p.cInstr, nil
+}
 
-	return &ci, nil
+func (p *Parser) setDest() error {
+	if p.cmdBuilder.Len() == 0 {
+		return &ParseError{Pos: p.pos, Msg: fmt.Sprintf("Dest must be set up before '%v'", compDelim)}
+	}
+	p.cInstr.Dest = p.cmdBuilder.String()
+	p.cmdBuilder.Reset()
+	return nil
+}
+
+func (p *Parser) setComp() error {
+	if p.cmdBuilder.Len() == 0 && p.cInstr.Dest == "" {
+		return &ParseError{Pos: p.pos, Msg: "Computation operator absent after Destination"}
+	}
+	p.cInstr.Comp = p.cmdBuilder.String()
+	p.cmdBuilder.Reset()
+	return nil
+}
+
+func (p *Parser) setJump() error {
+	if p.cmdBuilder.Len() == 0 {
+		return &ParseError{Pos: p.pos, Msg: fmt.Sprintf("Jump must be be set up after '%v'", jumpDelim)}
+	}
+	p.cInstr.Jump = p.cmdBuilder.String()
+	p.cmdBuilder.Reset()
+	return nil
+}
+
+func (p *Parser) parseComment() error {
+	// checking the next slash
+	nextSlash, _, err := p.rReader.ReadRune()
+	if nextSlash != inlineCommentDelim || err != nil {
+		return &ParseError{Pos: p.pos, Msg: "Expected '/' for the inline comment"}
+	}
+	return nil
 }
