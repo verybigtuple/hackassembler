d6c34327ca0c23bfda1678fa1c51d4b6f6dfbe54
verybigtuple <bigtuple@gmail.com>
2021-01-04 03:13:25 +0300

    Added custom RuneReader

--- parser/base.go
+++ parser/base.go
@@ -1,10 +1,14 @@
 package parser
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 //Parser interface
 type Parser interface {
-	Parse(s string) (interface{}, error)
+	Parse(s string) (*interface{}, error)
 }
 
 //ParseError implements error arisen while parsing
@@ -14,5 +18,32 @@ type ParseError struct {
 }
 
 func (e *ParseError) Error() string {
-	return fmt.Sprintf("Paring error at position %d: %s", e.Pos, e.Msg)
+	return fmt.Sprintf("Parsing error at position %d: %s", e.Pos, e.Msg)
+}
+
+type pRuneReader struct {
+	reader *strings.Reader
+	Pos    int
+}
+
+func newPRuneReader(s string) *pRuneReader {
+	rr := pRuneReader{reader: strings.NewReader(s)}
+	return &rr
+}
+
+func (rR *pRuneReader) ReadRune() (rune, int, error) {
+	r, s, err := rR.reader.ReadRune()
+	if err == nil {
+		rR.Pos++
+	}
+	return r, s, err
+}
+
+func (rR *pRuneReader) ReadAfterSpaces() (rune, int, error) {
+	for {
+		rv, s, err := rR.ReadRune()
+		if !unicode.IsSpace(rv) || err != nil {
+			return rv, s, err
+		}
+	}
 }
